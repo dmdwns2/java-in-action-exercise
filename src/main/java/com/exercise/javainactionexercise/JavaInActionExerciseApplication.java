ac4c2f4e51a1869a542b8e81ae1157bb3491f5bb
dmdwns2 <ssreki1@naver.com>
2022-11-30 14:23:38 +0900

    프로젝트 생성

---
+++ src/main/java/com/exercise/javainactionexercise/JavaInActionExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.javainactionexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaInActionExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaInActionExerciseApplication.class, args);
+    }
+
+}
