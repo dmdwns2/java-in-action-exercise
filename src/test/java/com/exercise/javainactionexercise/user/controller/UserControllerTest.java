3733665d2fc5d0331010e36586fcb082123ab6fd
dmdwns2 <ssreki1@naver.com>
2022-12-02 13:41:08 +0900

    User 회원가입 성공 테스트

---
+++ src/test/java/com/exercise/javainactionexercise/user/controller/UserControllerTest.java
@@ -0,0 +1,47 @@
+package com.exercise.javainactionexercise.user.controller;
+
+import com.exercise.javainactionexercise.user.dto.UserJoinRequest;
+import com.exercise.javainactionexercise.user.service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest // 목테스트를 할거다.
+class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    UserService userService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("회원가입 성공")
+    void join() throws Exception {
+
+        String userName = "Mbappe";
+        String password = "123qwe";
+
+        mockMvc.perform(post("/api/v1/users/join")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(new UserJoinRequest(userName, password))))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+}
