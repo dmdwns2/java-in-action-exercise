b20cf40641d446935fb18b5f6a03d287fdf2c0ee
dmdwns2 <ssreki1@naver.com>
2022-12-02 14:08:16 +0900

    User Exception 처리, yml

--- src/main/java/com/exercise/javainactionexercise/user/controller/UserController.java
+++ src/main/java/com/exercise/javainactionexercise/user/controller/UserController.java
@@ -1,18 +1,26 @@
 package com.exercise.javainactionexercise.user.controller;
 
 import com.exercise.javainactionexercise.user.dto.UserJoinRequest;
+import com.exercise.javainactionexercise.user.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
 
+    private final UserService userService;
+
     @PostMapping("/join")
     public ResponseEntity<String> join(@RequestBody UserJoinRequest dto){
+        userService.join(dto.getUserName(), dto.getPassword());
         return ResponseEntity.ok().body("회원가입이 성공했습니다.");
     }
+
+
 }
