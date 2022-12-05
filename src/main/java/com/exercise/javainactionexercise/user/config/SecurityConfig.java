e630357fe6f25d7f3fa447e2ed0f9f3a38bd86e3
dmdwns2 <ssreki1@naver.com>
2022-12-05 11:48:30 +0900

    Configuration 추가

---
+++ src/main/java/com/exercise/javainactionexercise/user/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.exercise.javainactionexercise.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Controller;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .httpBasic().disable()
+                .cors().and()
+                .authorizeRequests()
+                .antMatchers("/api/**").permitAll()
+                .antMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .build();
+    }
+}
